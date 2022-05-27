ba1f57203249ce37968f99f295b7f5d55b4dfb27
arafin0 <sultanularafin000@gmail.com>
2022-05-27 14:43:23 +0600

    dashboard added

--- src/App.js
+++ src/App.js
@@ -5,12 +5,22 @@ import { Route, Routes } from "react-router-dom";
 import Navbar from "./Pages/Shared/Navber";
 import Login from "./Pages/Login/Login";
 import SignUp from "./Pages/Login/SignUp";
+import Dashboard from "./Pages/Dashboard/Dashboard";
+import RequireAuth from "./Pages/Login/RequireAuth";
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
       <Routes>
+        <Route
+          path="dashboard"
+          element={
+            <RequireAuth>
+              <Dashboard />
+            </RequireAuth>
+          }
+        ></Route>
         <Route path="login" element={<Login />} />
         <Route path="signup" element={<SignUp />} />
       </Routes>
