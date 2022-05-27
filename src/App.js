d97b1e77f953aaf302e8b699aa7c8640dc653c2d
arafin0 <sultanularafin000@gmail.com>
2022-05-27 14:28:42 +0600

    login page signup page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 // import logo from "./logo.svg";
 // import "./App.css";
 
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./Pages/Shared/Navber";
+import Login from "./Pages/Login/Login";
+import SignUp from "./Pages/Login/SignUp";
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
+      <Routes>
+        <Route path="login" element={<Login />} />
+        <Route path="signup" element={<SignUp />} />
+      </Routes>
     </div>
   );
 }
